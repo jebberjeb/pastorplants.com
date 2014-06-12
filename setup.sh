d77907bbc1a1402e94f8284263e0facc42454e83
Jeb Beich <jebbeich@gmail.com>
2014-06-12 10:05:52 -0500

    adding apache to setup

--- setup.sh
+++ setup.sh
@@ -3,10 +3,12 @@ sudo systemctl set-default multi-user.target
 sudo yum install git
 sudo yum install tmux
 sudo yum groupinstall "Development Tools"
+
 mkdir ~/source
 cd ~/source
 git clone https://github.com/jebberjeb/scripts
 git clone https://github.com/jebberjeb/dotfiles
+
 cd ~
 rm .bashrc
 rm .bash_profile
@@ -20,3 +22,17 @@ ln -s ~/source/dotfiles/.tmux.conf .tmux.conf
 ~/source/scripts/install_jebs_vim.sh
 ~/source/scripts/vim-setup.sh
 
+sudo cp ~/source/scripts/google-chrome.repo /etc/yum.repos.d
+sudo yum install --nogpgcheck google-chrome-stable
+
+wget https://www.dropbox.com/sh/at2y14m65f12qe9/AAAX9amN5DyuoUu8AZPGJ1F5a/virtualbox-stuff/jdk-7u60-linux-x64.rpm
+sudo rpm -i jdk-7u60-linux-x64.rpm
+
+sudo rm /usr/bin/java
+sudo ln -s /usr/java/default/bin/java /usr/bin/java
+
+wget http://mirror.olnevhost.net/pub/apache/maven/maven-3/3.0.5/binaries/apache-maven-3.0.5-bin.tar.gz
+tar xvf apache-maven-3.0.5-bin.tar.gz
+sudo cp -rf apache-maven-3.0.5 /usr/local/
+rm -rf apache-maven-3.0.5
+rm apache-maven-3.0.5-bin.tar.gz
